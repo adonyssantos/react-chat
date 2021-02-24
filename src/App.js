556a373effa9046a0f5ff7a4475f718d4e90213e
Adonys <adonyssantoscemas@gmail.com>
2021-02-24 19:36:55 -0400

    Login - Limpieza del codigo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 //import { db } from "./config/firebase";
 import Button from "./components/Button";
-import { signInWithGoogle, signOut  } from './login';
+import { signInWithGoogle, signOut } from "./login";
 
 function App() {
- 
   return (
     <div>
-      {
-        user ? (
-          <>
-          <Button onClick={ signOut } > Sing in with Google</Button>
-           <p> Bienvenidos al CHAT!</p>
-           </>
-        ) : <Button onClick = {signInWithGoogle} > Sing in with Google </Button>
-        }
-        </div>
+      {user ? (
+        <>
+          <Button onClick={signOut}> Sing in with Google</Button>
+          <p> Bienvenidos al CHAT!</p>
+        </>
+      ) : (
+        <Button onClick={signInWithGoogle}> Sing in with Google </Button>
+      )}
+    </div>
   );
 }
 
 export default App;
-
