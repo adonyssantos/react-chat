c31acaca9808a1cc64e4f048fa1972956468ffc8
Rusbert8 <rusbertsanchez8@gmail.com>
2021-02-25 14:35:17 -1200

    Add query for massages from firestore

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,9 +1,31 @@
+import {useEffect, useState} from "react";
+import {db} from "../config/firebase";
+
 const Channel ({ user = null }) => {
 
     const [messages, setMessages] = useState([]);
 
-    return <ul></ul>;    
-    
-    };
-    
-    export default Channel;
+    useEffect(() => {
+
+    const query = db.collection("messages")
+                    .orderBy("createdAt")
+                    .limit(100);
+
+    const unsubscribe = query.onSnapshot (querySnapshot => { 
+
+        const data = querySnapshot.docs.map (doc => ({ 
+            ...doc.data(),
+            id: doc.id, 
+        }));
+
+    setMessages (data);
+
+    });
+
+    return unsubscribe;
+
+    },[])
+
+    return <ul></ul>;
+};
+export default Channel;
