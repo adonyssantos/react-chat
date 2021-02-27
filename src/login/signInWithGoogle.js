de6c2dd0d6a533956bbcd5f26264bfea8ce7097b
Adonys <adonyssantoscemas@gmail.com>
2021-02-27 19:05:23 -0400

    Cambios en los Imports

--- src/login/signInWithGoogle.js
+++ src/login/signInWithGoogle.js
@@ -1,15 +1,13 @@
-import { googleAuthProvider, firebase } from "./config/firebase";
-
+import { googleAuthProvider, firebase } from "../config/firebase";
 
 const signInWithGoogle = async () => {
-    firebase.auth().useDeviceLanguage();
+  firebase.auth().useDeviceLanguage();
 
-    try{
-      await firebase.auth().signInWithPopup(googleAuthProvider);
-
-    } catch (e) {
-      console.error (e.message);
-    }
+  try {
+    await firebase.auth().signInWithPopup(googleAuthProvider);
+  } catch (e) {
+    console.error(e.message);
   }
+};
 
-  export default signInWithGoogle;
+export default signInWithGoogle;
