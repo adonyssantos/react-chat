9d4bc21d71b0fb365addbc099bb374270e9d0e10
Estheixys Peña <pehinojosa72@gmail.com>
2021-03-02 12:22:00 -0400

    Add service Messenger

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,33 +1,104 @@
 import { useEffect, useState } from "react";
 import { db } from "../config/firebase";
+import Message from './Message';
 
 const Channel = ({ user = null }) => {
-  console.log(user);
+  //console.log(user);
 
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    const query = db.collection("messages").orderBy("createdAt").limit(100);
+  }, [])
 
-    const unsubscribe = query.onSnapshot((querySnapshot) => {
-      const data = querySnapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
+  const { uid, displayName, photoURL} = user;
+  const [ newMessage, setNewMessage ]= useState('');
+  const handleMessageOnChange = (e) =>{
+    e.preventDefault();
+    setNewMessage(e.target.value);
+  }
 
-      setMessages(data);
-    });
+  const messagesRef = db.collection('messages');
 
-    return unsubscribe;
-  }, []);
+  const handleOnSubmit = e =>{
+    e.preventDefault();
 
-  return (
-    <ul>
-      {messages.map((message) => (
-        <li key={message.id}>{message.text}</li>
-      ))}
-    </ul>
-  );
-};
+        const trimmedMessage = newMessage.trim(); 
+        if (trimmedMessage) {
+          messagesRef.add({
+            text: trimmedMessage,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            displayName,
+            photoURL,
+          });
+          setNewMessage('');
+        }
+      };
+  
 
+  
+
+const inputRef = useRef();
+useEffect(() => {
+    if (inputRef.current){
+        inputRef.current.focus();
+    }
+}, [inputRef]);
+
+
+    return(
+        <>
+            <ul>
+                {messages.map(message => (
+                    <li key={message.id}>
+                        <Message {...message} />
+                    </li>
+                ))}
+            </ul>
+
+            <form
+                onSubmit={handleOnSubmit}>
+                    <input
+                    ref={inputRef}
+                    type="text"
+                    value={newMessage}
+                    onChange={handleMessageOnChange}
+                    placeholder= "Escribe tu mensaje aqui..."
+                    />
+                    <button 
+                    type="submit"
+                    disabled={!newMessage}
+                    >
+                        Send
+                    </button>
+                </form>
+            </>
+    );
+ };
+
+  
 export default Channel;
+
+  //const query = db.collection("messages").orderBy("createdAt").limit(100);
+
+    //const unsubscribe = query.onSnapshot((querySnapshot) => {
+ //     const data = querySnapshot.docs.map((doc) => ({
+ //       ...doc.data(),
+ //       id: doc.id,
+ //     }));
+//
+ //     setMessages(data);
+ //   });
+//
+ //   return unsubscribe;
+ // }, []);
+//
+ // return (
+ //   <ul>
+ //     {messages.map((message) => (
+ //       <li key={message.id}>{message.text}</li>
+ //     ))}
+ //   </ul>
+ // );
+//};
+//
---
+++ src/components/Message.js
@@ -0,0 +1,42 @@
+import { formatRelative } from 'date-fns';
+
+const formatDate = date => {
+    let formattedDate = '';
+    if (date) {
+        formattedDate = formatRelative(date, new Date());
+
+        formattedDate = 
+            formattedDate.charAt(0).toUpperCase() + formatDate.slice(1);
+    }
+    return formattedDate;
+};
+
+const Message = ({
+    createdAt = null,
+    text = '',
+    displayName = '',
+    photoURL = '',
+
+}) => {
+    if (!text) return null;
+        return(
+            <div>
+                {photoURL ? (
+                    <img 
+                    src={photoURL}
+                    alt="Avatar"
+                    className="rounded-full mr-4"
+                    width={45}
+                    height={45}
+                    />
+                ) : null}
+                {displayName ? <p>{displayName}</p> : null}
+                {createdAt?.seconds ? (
+                    <span>{formatDate(new Date(createdAt,seconds * 1000))}</span>
+                ) : null}
+                <p>{text}</p>
+            </div>
+        );;
+};
+
+export default Message;
