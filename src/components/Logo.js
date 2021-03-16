4be832caf381124e68bd5096a56ff07d1b2e1a60
Adonys <adonysmundoteh@gmail.com>
2021-03-16 15:09:54 -0400

    Cambio de logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import '../styles/Logo.css';
 
 const Logo = () => {
   return (
-      <img src="https://i.imgur.com/k0lpehA.gif" alt=""></img>
+      <img src="https://i.imgur.com/hV5uceh.png" alt=""></img>
   );
 };
 
